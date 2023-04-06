598837174a04c0981b7dbace8640e5d2f09342c3
Habidhossen <habidhossen2@gmail.com>
2023-04-07 03:09:49 +0600

    AboutMe components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import AboutMe from "./components/AboutMe/AboutMe";
 import UserProfile from "./components/UserProfile/UserProfile";
 
 function App() {
   return (
     <div>
       <UserProfile />
+      <AboutMe />
     </div>
   );
 }
