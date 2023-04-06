34db64c618b4eee42ad92b7f8d7a7cb95b1db409
Habidhossen <habidhossen2@gmail.com>
2023-04-07 02:04:42 +0600

    initial setup done

---
+++ src/components/CipherMap/CipherMap.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CipherMap = () => {
+  return <div></div>;
+};
+
+export default CipherMap;
---
+++ src/components/PassSecurity/PassSecurity.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PassSecurity = () => {
+  return <div></div>;
+};
+
+export default PassSecurity;
